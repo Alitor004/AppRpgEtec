320fc84d334adafa394b8e30f3d9462256267c11
Alitor004 <irmaosgomes1302@gmail.com>
2023-09-27 22:30:18 -0300

    Listagem de Personagens

---
+++ AppRpgEtec/Models/Personagens/Personagem.cs
@@ -0,0 +1,26 @@
+﻿using AppRpgEtec.Models.Enuns;
+using AppRpgEtec.Models.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRpgEtec.Models.Personagens
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int PontosVida { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; }
+        public ClasseEnum Classe { get; set; }
+        public byte[] FotoPersonagem { get; set; }
+        public Usuario Usuario { get; set; }
+        public int Disputas { get; set; }
+        public int Vitorias { get; set; }
+        public int Derrotas { get; set; }
+    }
+}
--- AppRpgEtec/Views/Personagens/ListagemView.xaml.cs
+++ AppRpgEtec/Views/Personagens/ListagemView.xaml.cs
@@ -1,9 +1,16 @@
+using AppRpgEtec.ViewModels.Personagens;
+
 namespace AppRpgEtec.Views.Personagens;
 
 public partial class ListagemView : ContentPage
 {
-	public ListagemView()
-	{
-		InitializeComponent();
-	}
+    ListagemPersonagemViewModel viewModel;
+    public ListagemView()
+    {
+        InitializeComponent();
+
+        viewModel = new ListagemPersonagemViewModel();
+        BindingContext = viewModel;
+        Title = "Personagens - App Rpg Etec";
+    }
 }
